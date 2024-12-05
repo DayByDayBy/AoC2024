bbaff8627cb834575b9c8509649880d36a4e9e98
Diomedes Git <diomedesgit@gmail.com>
2024-12-05 20:52:34 +0000

    some bugfixes in 'clean' version

--- day01/clean.js
+++ day01/clean.js
@@ -5,20 +5,17 @@ const filePath = (__dirname, "small_data.txt");
 
 fs.readFile(filePath, "utf-8", (_, data) => {
 
-
   const pairs = data.trim().split("\n").map(line => line.trim().split(/\s+/).map(Number))
     .filter(pair => pair.length == 2 && pair.every(n => !isNaN(n)));
 
-  const sortedLeft = pairs.map(pair => pair[0].sort((a, b) => a-b));
-  const sortedRight = pairs.map(pair => pair[1].sort((a, b) => a-b));
+  const sortedLeft = pairs.map(pair => pair[0]).sort((a, b) => a-b);
+  const sortedRight = pairs.map(pair => pair[1]).sort((a, b) => a-b);
 
 
   const differences = sortedLeft.map((num, index) => Math.abs(num - sortedRight[index]));
   const occurrences = sortedRight.reduce((occ, val) => (occ[val] = (occ[val] || 0) + 1, occ), {});
   const totals = sortedLeft.map(value => value * (occurrences[value] || 0));
   
-
   console.log("difference_sum:", differences.reduce((a, b)=> a + b, 0));
   console.log("similarity score:", totals.reduce((a, b) => a + b, 0));
 });
-
