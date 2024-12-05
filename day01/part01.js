da3a5453143bdab538b4156d77b1da544201d633
Diomedes Git <diomedesgit@gmail.com>
2024-12-05 18:12:27 +0000

    couple of ways, think Math.abs is the one

--- day01/part01.js
+++ day01/part01.js
@@ -19,11 +19,26 @@ fs.readFile(filePath, "utf-8", (err, data) => {
         .split(/\s+/)
         .map(Number)).filter(pair => pair.length == 2 && pair.every(n => !isNaN(n)));
 
-//   console.log("pairs:", pairs);
+  console.log("pairs:", pairs);
+
+
+const leftNumSorted = pairs[0].sort()
+const rightNumSorted = pairs[1].sort()
+
+// const differences = leftNumSorted.map((num, index) => num - rightNumSorted[index]);
+
+// const differences = pairs.map(pair => pair[0] - pair[1]);
+
+
+const differences = leftNumSorted.map((num, index) => Math.abs(num - rightNumSorted[index]));
 
 
 
 
 
+
+
+console.log("differences:", differences);
 
 });
+
