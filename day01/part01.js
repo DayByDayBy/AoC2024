c8b3d5afea3930c4b93d40855e5ab0954c032001
Diomedes Git <diomedesgit@gmail.com>
2024-12-05 18:15:13 +0000

    couple of ways, think Math.abs is the one

--- day01/part01.js
+++ day01/part01.js
@@ -12,33 +12,18 @@ fs.readFile(filePath, "utf-8", (err, data) => {
   const pairs = data
     .trim()
     .split("\n")
-    .filter(line => line.trim() !='')
-    .map(line => 
-        line
-        .trim()
-        .split(/\s+/)
-        .map(Number)).filter(pair => pair.length == 2 && pair.every(n => !isNaN(n)));
+    .filter((line) => line.trim() != "")
+    .map((line) => line.trim().split(/\s+/).map(Number))
+    .filter((pair) => pair.length == 2 && pair.every((n) => !isNaN(n)));
 
   console.log("pairs:", pairs);
 
+  const leftNumSorted = pairs.map(pair => pair[0]).sort((a, b) => a-b);
+  const rightNumSorted = pairs.map(pair => pair[1]).sort((a, b) => a-b);
 
-const leftNumSorted = pairs[0].sort()
-const rightNumSorted = pairs[1].sort()
+  const differences = leftNumSorted.map((num, index) =>
+    Math.abs(num - rightNumSorted[index])
+  );
 
-// const differences = leftNumSorted.map((num, index) => num - rightNumSorted[index]);
-
-// const differences = pairs.map(pair => pair[0] - pair[1]);
-
-
-const differences = leftNumSorted.map((num, index) => Math.abs(num - rightNumSorted[index]));
-
-
-
-
-
-
-
-console.log("differences:", differences);
-
+  console.log("differences:", differences);
 });
-
