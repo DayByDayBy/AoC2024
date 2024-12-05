342b3389f10f4671eafe7a396e3c852686342ac5
Diomedes Git <diomedesgit@gmail.com>
2024-12-05 20:49:07 +0000

    some bugfixes in 'clean' version

--- day01/clean.js
+++ day01/clean.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = (__dirname, "data.txt");
+const filePath = (__dirname, "small_data.txt");
 
 fs.readFile(filePath, "utf-8", (_, data) => {
 
@@ -9,14 +9,16 @@ fs.readFile(filePath, "utf-8", (_, data) => {
   const pairs = data.trim().split("\n").map(line => line.trim().split(/\s+/).map(Number))
     .filter(pair => pair.length == 2 && pair.every(n => !isNaN(n)));
 
-  const [left, right] = [pairs.map(pair => pair[0]), pairs.map(pair => pair[1])];
+  const sortedLeft = pairs.map(pair => pair[0].sort((a, b) => a-b));
+  const sortedRight = pairs.map(pair => pair[1].sort((a, b) => a-b));
 
-  const differences = left.map((num, index) => Math.abs(num - right[index]));
-  const occurrences = right.reduce((occ, val) => (occ[val] = (occ[val] || 0) + 1, occ), {});
-  const totals = left.map(value => value * (occurrences[value] || 0));
+
+  const differences = sortedLeft.map((num, index) => Math.abs(num - sortedRight[index]));
+  const occurrences = sortedRight.reduce((occ, val) => (occ[val] = (occ[val] || 0) + 1, occ), {});
+  const totals = sortedLeft.map(value => value * (occurrences[value] || 0));
   
 
-  console.log("similarity score:", totals.reduce((a, b) => a + b, 0));
   console.log("difference_sum:", differences.reduce((a, b)=> a + b, 0));
+  console.log("similarity score:", totals.reduce((a, b) => a + b, 0));
 });
 
