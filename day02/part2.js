f3389b02bb4e6b78c60774e56d99586b72db1884
Diomedes Git <diomedesgit@gmail.com>
2024-12-06 02:14:51 +0000

    some more of part 2. not quite there, but saving game in case i go to bed

--- day02/part2.js
+++ day02/part2.js
@@ -11,28 +11,51 @@ fs.readFile(filePath, "utf-8", (err, data) => {
   const lines = data.trim().split("\n");
   const reports = lines.map((line) => line.split(" ").map(Number));
 
-  const isAscending = reports.map((row) =>
-    row
-      .slice(0, row.length - 1)
-      .every((value, i) => 
-        value < row[i + 1] && (row[i + 1] - value <= 3)
-      )
-  );
+  const limit = 3;
 
-  const isDescending = reports.map((row) =>
-    row
-      .slice(0, row.length - 1)
-      .every((value, i) => 
-        value > row[i + 1] && (value - row[i + 1] <= 3)
-      )
+
+  const isAscending = reports.map((row) => {
+    let errorCount = 0;
+    for (let i = 0; i < row.length; i++) {
+      if (row[i + 1] < row[i]) {
+        errorCount++;
+      }
+      if (errorCount > 1 || (errorCount === 1 && row[i + 1] - row[i] > 3)) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  const isDescending = reports.map((row) => {
+    let errorCount = 0;
+    for (let i = 0; i < row.length; i++) {
+      if (row[i + 1] > row[i]) {
+        errorCount++;
+      }
+      if (errorCount > 1 || (errorCount === 1 && row[i] - row[i + 1] > 3)) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+
+
+//   tis bit is me reducing memory complexity, probably not that necessary tbh
+
+const falseRows = reports.map((row, index) => 
+    !isAscending[index] && isAscending[index] ? index : -1).filter(index => index !=-1);
+
+const complexCheck = falseRows.map(index =>
+    isValidSequence(reports[index], limit, true) ||
+    isValidSequence(reports[index], limit, false)
   );
 
   const asCount = isAscending.filter(Boolean).length;
   const desCount = isDescending.filter(Boolean).length;
 
-  const safe = isAscending.map((val, idx) => 
-    val ||  isDescending[idx]);
-  
-  console.log(safe.filter(Boolean).length);
+  const safe = isAscending.map((val, idx) => val || isDescending[idx] || (complexCheck[falseRows.indexOf(idx)] || false));
 
+  console.log(safe.filter(Boolean).length);
 });
