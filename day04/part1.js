650e3ad492513ec4dceda37ea7b2a0d8f14ce32e
Diomedes Git <diomedesgit@gmail.com>
2024-12-07 06:31:24 +0000

    adding files etc for day 4

---
+++ day04/part1.js
@@ -0,0 +1,12 @@
+// 140 by 140 grid again
+// probably do the ole 2d array?
+
+//   maybe matching x and then checking adjacent squares for M
+//        - none, move on, 
+//        - find one, continue in that direction and check for an A, 
+//        - none, move on, check for S
+
+//      a bit slow, tho 
+//   maybe check for easy ones first? do a l-r pass, then a top-bottom pass? if so, probably double check xmas and samx?
+// 
+
