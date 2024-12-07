1bf72ed9a5fadce851252556caaf548f5854b2ec
Diomedes Git <diomedesgit@gmail.com>
2024-12-07 04:57:46 +0000

    bang, and the day is gone

--- day03/part2.js
+++ day03/part2.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 
-async function readAndSplitMulAndSum() {
+async function mull2() {
   const filePath = path.join(__dirname, "data.txt");
+  
 
   try {
     const fileContent = fs.readFileSync(filePath, "utf8");
+    const mull2 = fileContent.split('do()').map(track => track.split('don\'t()')[0]).join('');
+
     const regex = /mul\((\d+),(\d+)\)/g;
     let a, b, mulSum = 0;
 
-    while ((match = regex.exec(fileContent)) !== null) {
+    while ((match = regex.exec(mull2)) !== null) {
       const a = parseInt(match[1]);
       const b = parseInt(match[2]);
       const mulProd = a * b;
@@ -23,4 +26,4 @@ async function readAndSplitMulAndSum() {
     return ["oh no", "it no go"];
   }
 }
-readAndSplitMulAndSum().then((result) => console.log(result));
+mull2().then((result) => console.log(result));
