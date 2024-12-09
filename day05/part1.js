fc6e4339fe297b4599f8213a2512b865a9ac7336
Diomedes Git <diomedesgit@gmail.com>
2024-12-09 00:56:40 +0000

    adding some try/catch and error checking, going async

--- day05/part1.js
+++ day05/part1.js
@@ -1,34 +1,42 @@
-const { log } = require("console");
+const { log, error } = require("console");
 const fs = require("fs");
 const path = require("path");
-const filePath = path.join(__dirname, "small_data.txt");
 
-fs.readFile(filePath, "utf8", (err, data) => {
-  if (err) {
-    console.error("Error when reading file: ", err);
-    return;
-  }
 
-//   the two halves:
+async function checkUpdates(filename){
+    try{
+        const filePath = path.join(__dirname, filename);
+        fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) {error("Error when reading file: ", err);
+              return;
+            }
 
-  const parts = data.trim().split("\n\n");
+        //   the two parts:
+        const parts = data.trim().split("\n\n");
+        if (parts.length !==2){
+            throw new Error('expecting two parts');
+        }
 
-//   the ruleset:
+        //   the ruleset:
+        const rules = parts[0].trim().split("\n");
 
-  const rules = parts[0].trim().split("\n");
-  const rulesArray = rules.map((rule) => {
-    const [first, second] = rule
-    .split("|")
-    .map(Number);
-    return [first, second];
-  });
 
-//   the text to check:
-  const updates = parts[1].trim().split("\n");
-  const updatesArray = updates.map((update) => {
-    return update.split(',').map(Number);
-});
+        const rulesArray = rules.map((rule) => {
+            const [first, second] = rule.split("|").map(Number);
+            return [first, second];
+        });
 
-  log(rulesArray);
-  log(updatesArray);
-});
+        //   the text to check:
+        const updates = parts[1].trim().split("\n");
+        const updatesArray = updates.map((update) => {
+            return update.split(",").map(Number);
+        });
+
+        log(rulesArray);
+        log(updatesArray);
+        });
+        }
+
+
+
+// "small_data.txt"
