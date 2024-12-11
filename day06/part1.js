32c46b0bdf9d92f7b5981e1ec011f50d426c0704
Diomedes Git <diomedesgit@gmail.com>
2024-12-11 16:34:11 +0000

    workin on it

--- day06/part1.js
+++ day06/part1.js
@@ -23,6 +23,21 @@ const directions = [
     [1,0],
     [0,-1]
 ]
+let direction = directions[0];
+
+const guardFind(matrix, char) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === char) {
+                return [i,j];
+            }
+        }
+    }
+    return null;
+}
+
+const guardPos = guardFind(matrix, '^');
+
 
 // path check
 const isClear = (row, col, grid) =>{
@@ -33,7 +48,36 @@ const isClear = (row, col, grid) =>{
     );
 };
 
+// change direction
+function nextDirection(currentIndex) {
+    return (currentIndex + 1) % 4; // Move to the next direction, cycling back to 0 after 3
+}
 
 
 
 
+// const directionCalc = (position, directions){
+//     for (i=0; i< directions.length; i++) {
+//         const [dRow, dCol] = directions[i];
+//         if (!isClear && directions[0]){
+//             direction = direction[i+1]
+//     }
+    
+// }
+
+
+function pathFind(guardPos, grid){
+    while (
+        guardPos[0] > 0 && 
+        guardPos[0] < grid.length &&
+        guardPos[1] > 0 &&
+        guardPos[1] < grid[0].length){
+
+        }
+
+    if((guardPos + direction) === isClear){
+        guardPos = guardPos+direction;
+    } else {directionCalc}
+
+
+}
