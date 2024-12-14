751229431c32c51b27f980af3294e7ed24ebc1d2
Diomedes Git <diomedesgit@gmail.com>
2024-12-14 00:02:45 +0000

    here we go

--- day06/part2.js
+++ day06/part2.js
@@ -18,3 +18,21 @@ function isClear(){
 function walkGrid(){
 
 }
+
+function parseInput(input){
+    const lines = input.trim().split('\n');
+    const grid = lines.map(line => line.split('').map(char => char !== '#'));
+    let startPos = null;
+
+    for (let row=0; row< lines.length; row++) {
+        for (let col = 0; col < lines[row].length; col++){
+            if (lines[row][col] === '^'){
+                startPos = [row, col];
+            }
+        }
+    }
+     if (!startPos) {
+        throw new Error("guard (^) not found");
+     }
+return {grid, startPos};
+}
